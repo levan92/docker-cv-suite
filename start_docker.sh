1bd0dde8c3f1c473944d6d45ae3407af523d0fa2
levan92 <lingevan0208@gmail.com>
2021-06-29 23:44:27 +0800

    start docker args

--- start_docker.sh
+++ start_docker.sh
@@ -2,5 +2,12 @@
 export WORKSPACE=$HOME/Workspace
 export DATA=/media/dh/HDD/
 
+if [ -n "$1" ]; then
+    docker_image=$1
+else
+    docker_image="levan92/cv-suite:latest"
+fi
+echo "Running" $docker_image
+
 xhost +local:docker
-docker run -it --gpus all -w $WORKSPACE -v $WORKSPACE:$WORKSPACE -v $DATA:$DATA --net=host --ipc host -e DISPLAY=unix$DISPLAY  --privileged -v /dev/:/dev/ levan92/cv-suite
+docker run -it --gpus all -w $WORKSPACE -v $WORKSPACE:$WORKSPACE -v $DATA:$DATA --net=host --ipc host -e DISPLAY=unix$DISPLAY  --privileged -v /dev/:/dev/ $docker_image
